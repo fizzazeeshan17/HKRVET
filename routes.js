5c7cfa19690f4b7428f5af11c79398029a7e95ec
Bin-Umar Saiid <saiid337@yahoo.com>
2022-04-24 19:53:59 +0200

    .

--- routes.js
+++
@@ -1,5 +0,0 @@
-
-const express = require("express");
-
-const app = express(); //this means that it creates an instance of Express.
-
--- routes/secure.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const verify = require("./verifyToken");
-
-router.get("/", verify, (req, res) => {
-  res.redirect("/batcave");
-});
-module.exports = router;
