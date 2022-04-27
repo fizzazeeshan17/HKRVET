872fd16e1e2031eccb20b49a8bae09f46b41c8b8
EWNETU\Ewnetu <ewnetu1919@gmail.com>
2022-04-27 18:37:19 +0200

    just did the batcave.html

--- public/secure.js
+++ public/secure.js
@@ -6,4 +6,30 @@ fetch('/api/secure', {
 
     
 });
+const bookingForm = document.querySelector("#bookingForm")
+const dateInput = document.querySelector("#dateInput")
+const textareaInput = document.querySelector("#textareaInput")
+const error = document.querySelector("#error")
+
+
+bookingForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+
+    if (dateInput.value=="" || textareaInput.value=="" ){
+       error.innerHTML = "you have entered some values empty"
+       setTimeout(()=>{error.innerHTML=""}, 2000)
+       return
+    }
+
+    let data = {
+        dateValue : dateInput.value ,
+        textareaValue : textareaInput.value
+    }
+    
+
+    
+console.log('I am connected to the html file, here is your data', data )
+   
+})
 
