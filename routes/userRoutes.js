64d5bdf5e8d0829fdc9d78a85cd68bf6c79a0157
fizzazeeshan17 <fizzazeeshan123@gmail.com>
2022-04-29 22:23:11 +0200

    designed tables for administrator

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -30,7 +30,7 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.put("/:id", async (req, res) => {
+router.patch("/:id", async (req, res) => {
   const { phone, email, password } = req.body;
 
   try {
